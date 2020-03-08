699b8c3041bd36dff509be61a9c637bd862834f5
hanaKim <60841357+kkim218@users.noreply.github.com>
2020-03-08 23:14:48 +0900

    두 정수 사이의 합

---
+++ kkim218/Level.1/두_정수_사이의_합.java
@@ -0,0 +1,16 @@
+class Solution {
+  public long solution(int a, int b) {
+      long answer = 0;
+      
+      if ((a > -10000000 && a < 10000000) && (b > -10000000 && b < 10000000)) {
+            if (a == b)
+              return a;
+
+          for (int i = Math.min(a, b); i <= Math.max(a, b); i++) {
+              answer += i;
+          }
+      }
+      
+      return answer;
+  }
+}
