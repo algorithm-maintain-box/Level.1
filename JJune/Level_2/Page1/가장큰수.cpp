94907c2bb61e2e45d8bfc010084b344e1edfc341
JJune <asplannedl@gmail.com>
2020-03-16 19:53:44 +0900

    Add solution

--- JJune/Level_2/Page1/가장큰수.cpp
+++ JJune/Level_2/Page1/가장큰수.cpp
@@ -1,52 +1,59 @@
 #include <string>
 #include <vector>
-#include <map>
+#include <algorithm>
 
 using namespace std;
 
 string solution(vector<int> numbers)
 {
-    map<int, vector<string>> m{};
-
-    for (size_t i = 0; i < numbers.size(); i++)
+#if 1
+    vector<string> vec{};
+    for (auto iter : numbers)
     {
-        if (!numbers[i])
-        {
-            m[i].push_back("0");
-            continue;
-        }
+        vec.push_back(to_string(iter));
+    }
 
-        if (numbers[i] < 10)
-        {
-            m[numbers[i]].push_back(to_string(numbers[i]));
-        }
-        else if (numbers[i] < 100)
-        {
-            m[numbers[i] / 10].push_back(to_string(numbers[i]));
-        }
-        else
-        {
-            m[numbers[i] / 100].push_back(to_string(numbers[i]));
-        }
+    sort(vec.begin(),
+         vec.end(),
+         [](string a, string b) {
+             return a + b > b + a ? true : false;
+         });
+
+    if (vec[0] == "0")
+    {
+        return "0";
     }
 
     string result{};
-    for (auto i = 9; i > 0; i--)
+    for (auto iter : vec)
     {
-        sort(m[i].begin(), m[i].end());
-        for (auto iter : m[i])
-        {
-            result += iter;
-        }
+        result += iter;
     }
 
+#else
+    string result{};
+    sort(numbers.begin(), numbers.end());
+
+    do
+    {
+        string temp{};
+        for (auto i = 0; i < numbers.size(); i++)
+        {
+            temp += to_string(numbers[i]);
+        }
+
+        result = max(result, temp);
+    } while (next_permutation(numbers.begin(), numbers.end()));
+
+#endif
     return result;
 }
 
 #include <iostream>
 int main()
 {
-    std::cout << "MINE : " << solution({98, 9, 91, 93, 90}) << " A : 999891" << std::endl;
+    std::cout << "MINE : " << solution({0, 0, 0, 0, 0}) << " A : 0" << std::endl;
+    std::cout << "MINE : " << solution({98, 9, 91, 9}) << " A : 999891" << std::endl;
     std::cout << "MINE : " << solution({6, 10, 2}) << " A : 6210" << std::endl;
     std::cout << "MINE : " << solution({3, 30, 34, 5, 9}) << " A : 9534330" << std::endl;
     return 0;
---
+++ JJune/Level_2/Page1/숫자야구.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> baseball)
+{
+    vector<string> vec{};
+    for (auto i = 123; i <= 987; i++)
+    {
+        auto play_case = to_string(i);
+
+        if (play_case.find("0") != string::npos || (play_case[0] == play_case[1] || play_case[1] == play_case[2] || play_case[0] == play_case[2]))
+        {
+            continue;
+        }
+
+        vec.push_back(play_case);
+    }
+
+    for (auto iter : baseball)
+    {
+        auto test_case = to_string(iter[0]);
+        for (int i = vec.size() - 1; i >= 0; i--)
+        {
+            auto strick = 0;
+            auto ball = 0;
+
+            for (auto k = 0; k < 3; k++)
+            {
+                if (test_case[k] == vec[i][k])
+                {
+                    strick++;
+                }
+                else if (test_case[k] == vec[i][(k + 1) % 3] || test_case[k] == vec[i][(k + 2) % 3])
+                {
+                    ball++;
+                }
+            }
+
+            if (iter[1] != strick || iter[2] != ball)
+            {
+                vec.erase(vec.begin() + i);
+            }
+        }
+
+        return vec.size();
+    }
+}
+
+#include <iostream>
+int main()
+{
+    std::cout << "MINE : " << solution({{123, 1, 1}, {356, 1, 0}, {327, 2, 0}, {489, 0, 1}}) << " A : 2" << std::endl;
+    return 0;
+}
