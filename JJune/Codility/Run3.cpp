dd9de77a24d7ee8893c5520f85d3a7290948c060
Window10 <asplannedl@gmail.com>
2020-03-27 13:22:02 +0900

    Add run file

---
+++ JJune/Codility/Run3.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main()
+{
+    // std::vector<int> vec{3, 1, 2, 4, 3};
+    // std::cout << solution(vec) << " / A : 1" << std::endl;
+
+    std::cout << "Test run3 file" << std::endl;
+}
